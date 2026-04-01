f31889e521404299c3775818f2a0dbc46437337b
ezzudin <ezzudintomizi@gmail.com>
2026-04-01 16:49:28 +0300

    Opcode table has been built

---
+++ op_codes/op_table.py
@@ -0,0 +1,217 @@
+op_table = {
+    "ADD":
+    {
+        "opcode": 0x19,
+        "operand_exits": True
+    },
+    "ADDF":
+    {
+        "opcode": 0x58,
+        "operand_exits": True
+    },
+    "AND":
+    {
+        "opcode": 0x40,
+        "operand_exits": True
+    },
+    "COMP":
+    {
+        "opcode": 0x28,
+        "operand_exits": True
+    },
+    "COMPF":
+    {
+        "opcode": 0x88,
+        "operand_exits": True
+    },
+    "DIV":
+    {
+        "opcode": 0x24,
+        "operand_exits": True
+    },
+    "DIVF":
+    {
+        "opcode": 0x64,
+        "operand_exits": True
+    },
+    "FIX":
+    {
+        "opcode": 0xC4,
+        "operand_exits": False
+    },
+    "FLOAT":
+    {
+        "opcode": 0xC0,
+        "operand_exits": False
+    },
+    "HIO":
+    {
+        "opcode": 0xF4,
+        "operand_exits": False
+    },
+    "J":
+    {
+        "opcode": 0x3C,
+        "operand_exits": True
+    },
+    "JEQ":
+    {
+        "opcode": 0x30,
+        "operand_exits": True
+    },
+    "JGT":
+    {
+        "opcode": 0x34,
+        "operand_exits": True
+    },
+    "JLT":
+    {
+        "opcode": 0x38,
+        "operand_exits": True
+    },
+    "JSUB":
+    {
+        "opcode": 0x48,
+        "operand_exits": True
+    },
+    "LDA":
+    {
+        "opcode": 0x00,
+        "operand_exits": True
+    },
+    "LDB":
+    {
+        "opcode": 0x68,
+        "operand_exits": True
+    },
+    "LDCH":
+    {
+        "opcode": 0x50,
+        "operand_exits": True
+    },
+    "LDF":
+    {
+        "opcode": 0x70,
+        "operand_exits": True
+    },
+    "LDL":
+    {
+        "opcode": 0x08,
+        "operand_exits": True
+    },
+    "LDS":
+    {
+        "opcode": 0x6C,
+        "operand_exits": True
+    },
+    "LDT":
+    {
+        "opcode": 0x74,
+        "operand_exits": True
+    },
+    "LDX":
+    {
+        "opcode": 0x04,
+        "operand_exits": True
+    },
+    "LPS":
+    {
+        "opcode": 0xD0,
+        "operand_exits": True
+    },
+    "MUL":
+    {
+        "opcode": 0x20,
+        "operand_exits": True
+    },
+    "MULF":
+    {
+        "opcode": 0x60,
+        "operand_exits": True
+    },
+    "NORM":
+    {
+        "opcode": 0xC8,
+        "operand_exits": False
+    },
+    "OR":
+    {
+        "opcode": 0x44,
+        "operand_exits": True
+    },
+    "RD":
+    {
+        "opcode": 0xD8,
+        "operand_exits": True
+    },
+    "RSUB":
+    {
+        "opcode": 0x4B,
+        "operand_exits": False
+    },
+    "SIO":
+    {
+        "opcode": 0xF0,
+        "operand_exits": False
+    },
+    "SSK":
+    {
+        "opcode": 0xEC,
+        "operand_exits": True
+    },
+    "STA":
+    {
+        "opcode": 0x0C,
+        "operand_exits": True
+    },
+    "STB":
+    {
+        "opcode": 0x78,
+        "operand_exits": True
+    },
+    "STCH":
+    {
+        "opcode": 0x54,
+        "operand_exits": True
+    },
+    "STT":
+    {
+        "opcode": 0x84,
+        "operand_exits": True
+    },
+    "STX":
+    {
+        "opcode": 0x10,
+        "operand_exits": True
+    },
+    "SUB":
+    {
+        "opcode": 0x1C,
+        "operand_exits": True
+    },
+    "SUBF":
+    {
+        "opcode": 0x5C,
+        "operand_exits": True
+    },
+    "TD":
+    {
+        "opcode": 0xE0,
+        "operand_exits": True
+    },
+    "TIO":
+    {
+        "opcode": 0xF8,
+        "operand_exits": False
+    },
+    "TIX":
+    {
+        "opcode": 0x2C,
+        "operand_exits": True
+    },
+    "WD":
+    {
+        "opcode": 0xDC,
+        "operand_exits": True
+    }
+}
